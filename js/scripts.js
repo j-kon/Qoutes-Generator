0c3fa2a7acbd41548b3c20b81341aea13a97aa00
Jeremiah Jacob <jaykon4me@gmail.com>
2019-10-30 12:28:31 +0100

    Update scripts.js
    added a quote by Arthur Schopenhauer

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,7 @@ var quotes = [
 	['Memories of our lives, of our work and our deeds will continue in others.', 'Rosa Parks'],
 	['No matter what people tell you, words and ideas can change the world.', 'Robin Williams'],
 	['I believe in living today. Not in yesterday, nor in tomorrow.', 'Loretta Young']
+	['The two enemies of human happiness are pain and boredom.', 'Arthur Schopenhauer'],
 ];
 
 // ** - indicates code hasn't been written yet
